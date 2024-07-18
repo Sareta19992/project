c292adb24c352b5cafb6abf464570be10b0e4be3
Sareta19992 <shamolei@my.centennialcollege.ca>
2024-07-17 21:00:19 -0400

    first commit

--- node_modules/group-project/controllers/weather.js
+++ node_modules/group-project/controllers/weather.js
@@ -1,61 +1,10 @@
-// const WeatherReport = require('../models/WeatherReport');
-
-// exports.createWeatherReport = async (req, res) => {
-//     try {
-//         const weatherReport = new WeatherReport(req.body);
-//         await weatherReport.save();
-//         res.status(201).send(report);
-//     } catch (error) {
-//         res.status(400).send(error);
-//     }
-// };
-
-// exports.getWeatherReports = async (req, res) => {
-//     try {
-//         const weatherReports = await WeatherReport.find();
-//         res.send(weatherReports);
-//     } catch (error) {
-//         res.status(500).send(error);
-//     }
-// };
-
-// exports.getWeatherReport = async (req, res) => {
-//     try {
-//         console.log('getWeatherReport called with ID:', req.params.id);
-//         const weatherReport = await WeatherReport.findById(req.params.id);
-//         if (!weatherReport) return res.status(404).send();
-//         res.send(weatherReport);
-//     } catch (error) {
-//         res.status(500).send(error);
-//     }
-// };
-
-// exports.updateWeatherReport = async (req, res) => {
-//     try {
-//         const weatherReport = await WeatherReport.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-//         if (!weatherReport) return res.status(404).send();
-//         res.send(weatherReport);
-//     } catch (error) {
-//         res.status(400).send(error);
-//     }
-// };
-
-// exports.deleteWeatherReport = async (req, res) => {
-//     try {
-//         const weatherReport = await WeatherReport.findByIdAndDelete(req.params.id);
-//         if (!weatherReport) return res.status(404).send();
-//         res.send({ message: "Delete successful" });
-//     } catch (error) {
-//         res.status(500).send(error);
-//     }
-// };
 const WeatherReport = require('../models/WeatherReport');
 
 exports.createWeatherReport = async (req, res) => {
     try {
-        const report = new WeatherReport(req.body);
-        await report.save();
-        res.status(201).send(report);
+        const weatherReport = new WeatherReport(req.body);
+        await weatherReport.save();
+        res.status(201).send(weatherReport);
     } catch (error) {
         res.status(400).send(error);
     }
@@ -63,7 +12,6 @@ exports.createWeatherReport = async (req, res) => {
 
 exports.getWeatherReports = async (req, res) => {
     try {
-
         const weatherReports = await WeatherReport.find();
         res.send(weatherReports);
     } catch (error) {
@@ -74,9 +22,9 @@ exports.getWeatherReports = async (req, res) => {
 exports.getWeatherReport = async (req, res) => {
     try {
         console.log('getWeatherReport called with ID:', req.params.id); // Debug log
-        const report = await WeatherReport.findById(req.params.id);
-        if (!report) return res.status(404).send();
-        res.send(report);
+        const weatherReport = await WeatherReport.findById(req.params.id);
+        if (!weatherReport) return res.status(404).send();
+        res.send(weatherReport);
     } catch (error) {
         res.status(500).send(error);
     }
@@ -84,9 +32,9 @@ exports.getWeatherReport = async (req, res) => {
 
 exports.updateWeatherReport = async (req, res) => {
     try {
-        const report = await WeatherReport.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!report) return res.status(404).send();
-        res.send(report);
+        const weatherReport = await WeatherReport.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!weatherReport) return res.status(404).send();
+        res.send(weatherReport);
     } catch (error) {
         res.status(400).send(error);
     }
@@ -94,8 +42,8 @@ exports.updateWeatherReport = async (req, res) => {
 
 exports.deleteWeatherReport = async (req, res) => {
     try {
-        const report = await WeatherReport.findByIdAndDelete(req.params.id);
-        if (!report) return res.status(404).send();
+        const weatherReport = await WeatherReport.findByIdAndDelete(req.params.id);
+        if (!weatherReport) return res.status(404).send();
         res.send({ message: "Delete successful" });
     } catch (error) {
         res.status(500).send(error);
